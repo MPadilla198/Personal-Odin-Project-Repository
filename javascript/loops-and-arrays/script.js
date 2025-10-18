ecab8090583d040a077cbb346ac7a749a85b491e
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 00:31:43 -0500

    Complete filter range exercise

--- javascript/loops-and-arrays/script.js
+++ javascript/loops-and-arrays/script.js
@@ -15,3 +15,16 @@ console.log(camelize("list-style-image"))
 console.assert(camelize("list-style-image") == 'listStyleImage');
 console.log(camelize("-webkit-transition"))
 console.assert(camelize("-webkit-transition") == 'WebkitTransition');
+
+
+// https://javascript.info/array-methods#filter-range
+function filterRange(arr, a ,b) {
+    return arr.filter((val) => val >= a && val <= b)
+}
+
+// Test
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (matching values)
+alert( arr ); // 5,3,8,1 (not modified)
