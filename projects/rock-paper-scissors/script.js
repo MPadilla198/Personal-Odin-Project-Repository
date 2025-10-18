4b6dc71bad81e17a5eaa0c34a6deb74726f2520c
Miguel Padilla <mpadi21@mit.edu>
2025-10-17 23:10:55 -0500

    Add getHumanChoice function to rock-paper-scissors project

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -2,3 +2,6 @@ function getComputerChoice() {
     return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)]
 }
 
+function getHumanChoice() {
+    return prompt("Enter your choice between rock, paper, and scissors: ")
+}
