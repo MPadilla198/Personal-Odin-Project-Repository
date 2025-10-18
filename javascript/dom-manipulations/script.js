90e25683fa83aa62a753a98c016c3a3e60450198
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 17:26:51 -0500

    Complete DOM manipulation exercise 2

--- javascript/dom-manipulations/script.js
+++ javascript/dom-manipulations/script.js
@@ -12,3 +12,10 @@ paragraph.style.color = "red"
 paragraph.textContent = "Hey I'm red!"
 
 container.appendChild(paragraph)
+
+// 2
+const heading3 = document.createElement("h3")
+heading3.style.color = "blue"
+heading3.textContent = "I'm a blue h3!"
+
+container.appendChild(heading3)
