86164cf9c95ab0014cbc5b992ecdc5d45ae4b055
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 19:55:54 -0500

    Add div for displaying results and replaced console logs with div's textContent

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -11,20 +11,20 @@ let humanScore = 0
 let computerScore = 0
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase()
+    const resultsDiv = document.querySelector("#results")
 
     switch (humanChoice) {
         case "rock":
             switch (computerChoice) {
                 case "rock":
-                    console.log("It's a tie! Neither of you get points.")
+                    resultsDiv.textContent = "It's a tie! Neither of you get points."
                     break
                 case "paper":
-                    console.log("You lose! Paper beats rock.")
+                    resultsDiv.textContent = "You lose! Paper beats rock."
                     computerScore++
                     break
                 case "scissors":
-                    console.log("You win! Rock beats scissors.")
+                    resultsDiv.textContent = "You win! Rock beats scissors."
                     humanScore++
                     break
             }
@@ -32,14 +32,14 @@ function playRound(humanChoice, computerChoice) {
         case "paper":
             switch (computerChoice) {
                 case "rock":
-                    console.log("You win! Paper beats rock.")
+                    resultsDiv.textContent = "You win! Paper beats rock."
                     humanScore++
                     break
                 case "paper":
-                    console.log("It's a tie! Neither of you get points.")
+                    resultsDiv.textContent = "It's a tie! Neither of you get points."
                     break
                 case "scissors":
-                    console.log("You lose! Scissors beats paper.")
+                    resultsDiv.textContent = "You lose! Scissors beats paper."
                     computerScore++
                     break
             }
@@ -47,20 +47,20 @@ function playRound(humanChoice, computerChoice) {
         case "scissors":
             switch (computerChoice) {
                 case "rock":
-                    console.log("You lose! Rock beats scissors.")
+                    resultsDiv.textContent = "You lose! Rock beats scissors."
                     computerScore++
                     break
                 case "paper":
-                    console.log("You win! Scissors beats paper.")
+                    resultsDiv.textContent = "You win! Scissors beats paper."
                     humanScore++
                     break
                 case "scissors":
-                    console.log("It's a tie! Neither of you get points.")
+                    resultsDiv.textContent = "It's a tie! Neither of you get points."
                     break
             }
             break
         default:
-            console.log(humanChoice + " is not an option. Please, try again.")
+            resultsDiv.textContent = humanChoice + " is not an option. Please, try again."
             break
     }
 }
