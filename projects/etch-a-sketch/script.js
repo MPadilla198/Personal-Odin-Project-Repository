79dbc2fa98b760239548fe02d9ea243900e64257
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 22:06:16 -0500

    Add equal sized divs that cover the page

---
+++ projects/etch-a-sketch/script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container")
+
+const SIZE = 16
+
+for (let i = 0; i < SIZE; i++) {
+    const row = document.createElement("div")
+    row.classList.add("row")
+
+    for (let j = 0; j < SIZE; j++) {
+        const column = document.createElement("div")
+        column.classList.add("col")
+
+        row.appendChild(column)
+    }
+
+    container.appendChild(row)
+}
