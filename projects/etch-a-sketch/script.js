1ececc15dcd4946f6831a2996a724aa2da34c200
Miguel Padilla <mpadi21@mit.edu>
2025-10-19 00:16:14 -0500

    Add button to resize the pixels on screen from the etch a asketch

--- projects/etch-a-sketch/script.js
+++ projects/etch-a-sketch/script.js
@@ -1,21 +1,44 @@
 const container = document.querySelector(".container")
 
-const SIZE = 16
+const resetButton = document.querySelector("#reset-button")
 
-for (let i = 0; i < SIZE; i++) {
-    const row = document.createElement("div")
-    row.classList.add("row")
+resetButton.addEventListener("click", (event) => {
+    const size = prompt("How many squares per side? (Limit: 100)")
+    if (size <= 0) {
+        alert("The number of boxes per side may not be less than or equal to zero.")
+    } else if (size > 100) {
+        alert("The limit of the number of boxes per side is 100. Please, choose a number less than or equal to 100.")
+    } else {
+        buildScreen(size)
+    }
+})
 
-    for (let j = 0; j < SIZE; j++) {
-        const column = document.createElement("div")
-        column.classList.add("col")
-
-        column.addEventListener("mouseenter", (event) => {
-            column.style.backgroundColor = "blue"
-        })
-
-        row.appendChild(column)
+function buildScreen(size) {
+    // Clear the board
+    while (container.firstChild) {
+        container.removeChild(container.lastChild)
     }
 
-    container.appendChild(row)
+    // Add new
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div")
+        row.classList.add("row")
+
+        for (let j = 0; j < size; j++) {
+            const column = document.createElement("div")
+            column.classList.add("col")
+
+            column.addEventListener("mouseenter", (event) => {
+                column.style.backgroundColor = "blue"
+            })
+
+            row.appendChild(column)
+        }
+
+        container.appendChild(row)
+    }
 }
+
+const SIZE = 16
+
+buildScreen(SIZE)
