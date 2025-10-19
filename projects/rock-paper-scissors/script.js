60773325c192905d3f80d215962f1a7b700e8be9
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 19:49:58 -0500

    Add event listeners to rpss buttons to play a round when clicked

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -7,73 +7,80 @@ function getHumanChoice() {
 }
 
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+let humanScore = 0
+let computerScore = 0
 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase()
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
 
-        switch (humanChoice) {
-            case "rock":
-                switch (computerChoice) {
-                    case "rock":
-                        console.log("It's a tie! Neither of you get points.")
-                        break
-                    case "paper":
-                        console.log("You lose! Paper beats rock.")
-                        computerScore++
-                        break
-                    case "scissors":
-                        console.log("You win! Rock beats scissors.")
-                        humanScore++
-                        break
-                }
-                break
-            case "paper":
-                switch (computerChoice) {
-                    case "rock":
-                        console.log("You win! Paper beats rock.")
-                        humanScore++
-                        break
-                    case "paper":
-                        console.log("It's a tie! Neither of you get points.")
-                        break
-                    case "scissors":
-                        console.log("You lose! Scissors beats paper.")
-                        computerScore++
-                        break
-                }
-                break
-            case "scissors":
-                switch (computerChoice) {
-                    case "rock":
-                        console.log("You lose! Rock beats scissors.")
-                        computerScore++
-                        break
-                    case "paper":
-                        console.log("You win! Scissors beats rock.")
-                        humanScore++
-                        break
-                    case "scissors":
-                        console.log("It's a tie! Neither of you get points.")
-                        break
-                }
-                break
-            default:
-                console.log(humanChoice + " is not an option. Please, try again.")
-                break
-        }
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("It's a tie! Neither of you get points.")
+                    break
+                case "paper":
+                    console.log("You lose! Paper beats rock.")
+                    computerScore++
+                    break
+                case "scissors":
+                    console.log("You win! Rock beats scissors.")
+                    humanScore++
+                    break
+            }
+            break
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("You win! Paper beats rock.")
+                    humanScore++
+                    break
+                case "paper":
+                    console.log("It's a tie! Neither of you get points.")
+                    break
+                case "scissors":
+                    console.log("You lose! Scissors beats paper.")
+                    computerScore++
+                    break
+            }
+            break
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("You lose! Rock beats scissors.")
+                    computerScore++
+                    break
+                case "paper":
+                    console.log("You win! Scissors beats paper.")
+                    humanScore++
+                    break
+                case "scissors":
+                    console.log("It's a tie! Neither of you get points.")
+                    break
+            }
+            break
+        default:
+            console.log(humanChoice + " is not an option. Please, try again.")
+            break
     }
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection)
-    }
-
-    console.log("Human score: " + humanScore)
-    console.log("Computer score: " + computerScore)
 }
 
-// playGame()
+let buttonRow = document.querySelector('#button-row')
+
+buttonRow.addEventListener("click", function (event) {
+    const target = event.target
+
+    const computerSelection = getComputerChoice()
+
+    switch (target.id) {
+        case "rock":
+            playRound("rock", computerSelection)
+            break
+        case "paper":
+            playRound("paper", computerSelection)
+            break
+        case "scissors":
+            playRound("scissors", computerSelection)
+            break
+    }
+})
